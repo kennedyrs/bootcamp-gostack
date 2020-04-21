543a846727dea2aeca7f3cbc48f05f52631109e5
kennedyrs <kennedywork202@gmail.com>
2020-04-21 17:05:14 -0400

    Mobile React Native

---
+++ nivel01/03-mobile-com-react-native/mobile/src/index.js
@@ -0,0 +1,83 @@
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('/projects', {
+      title: `Date now ${Date.now()}`,
+      owner: 'Kennedy',
+    });
+
+    setProjects([response.data, ...projects]);
+  }
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.title}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
+
+        {/* <View style={styles.container}>
+        <Text style={styles.title}>Olá Screen</Text>
+      </View> */}
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+
+  title: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});

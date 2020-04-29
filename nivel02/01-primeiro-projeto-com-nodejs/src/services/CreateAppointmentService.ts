1cadde611e5cd0bbb189bf66f9acfe4a9d01979e
kennedyrs <kennedywork202@gmail.com>
2020-04-28 22:27:25 -0400

    Primeiro projeto com Node.JS

---
+++ nivel02/01-primeiro-projeto-com-nodejs/src/services/CreateAppointmentService.ts
@@ -0,0 +1,38 @@
+import { startOfHour } from 'date-fns'
+import AppointmentsRepository from '../repositories/AppointmentsRepository'
+
+import Appointment from '../models/Appointment'
+
+interface Request {
+  provider: string
+  date: Date
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date)
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    )
+
+    if (findAppointmentInSameDate) {
+      throw Error('This appointment has already bean taked')
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    })
+
+    return appointment
+  }
+}
+
+export default CreateAppointmentService
